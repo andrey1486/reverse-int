3c3bb8b2ba28ddb2fbb06dab63754ece431c64b3
Andrey <88458447+andrey1486@users.noreply.github.com>
2023-02-16 11:26:32 +0200

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = '';
+
+    n = String(Math.abs(n));
+    while (n[n.length - 1] === '0') {
+        n = n.slice(0, n.length - 1);
+    }
+    for (let i of n) {res = `${i}${res}`;}
+    
+    return res;
 }
